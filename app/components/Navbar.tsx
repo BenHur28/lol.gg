57b8582be568866684c1ca929bc24af0525e2dcb
Ben Hur <Bhur777@gmail.com>
2023-07-26 23:37:11 -0400

    Add icons and buttons to navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,3 +1,22 @@
+import { BsFillGearFill } from "react-icons/bs";
+import { AiTwotoneMail } from "react-icons/Ai";
+
 export default function Navbar() {
-	return <nav className="w-full bg-nav h-20 border-b border-b-border"></nav>;
+	return (
+		<nav className="w-full bg-nav h-20 border-b border-b-border">
+			<div className="h-full flex flex-row text-white justify-end items-center">
+				<div className="flex flex-row gap-4 items-center">
+					<button>
+						<BsFillGearFill className="h-5 w-5" />
+					</button>
+					<button>
+						<AiTwotoneMail className="h-5 w-5" />
+					</button>
+					<button className="text-white text-sm bg-blue-500 rounded-sm px-6 py-2 mr-10">
+						Log In
+					</button>
+				</div>
+			</div>
+		</nav>
+	);
 }
