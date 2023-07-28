f5c3dcbc9470eae6c9f5180738c1ee9e556d6651
Ben Hur <Bhur777@gmail.com>
2023-07-27 22:51:55 -0400

    Fetch and filter items data for 5v5 map only

--- app/items/page.tsx
+++ app/items/page.tsx
@@ -12,15 +12,29 @@ export default async function ItemsPage() {
 	const res = await itemData;
 	const items: Item[] = [];
 	for (let item in res.data) {
-		if (res.data[item].inStore != false) {
+		if (
+			(res.data[item].inStore != false || undefined) &&
+			res.data[item].maps[11] === true &&
+			!res.data[item].requiredChampion &&
+			res.data[item].name !== "Obsidian Edge"
+		) {
 			items.push(res.data[item]);
 		}
 	}
+	console.log(items.length);
 	return (
 		<div className="flex flex-col items-center text-white text-center">
 			<ul>
 				{items.map((item) => (
-					<li key={item.name}>{item.name}</li>
+					<li key={item.name}>
+						<img
+							src={`http://ddragon.leagueoflegends.com/cdn/13.14.1/img/item/${item.image.full}`}
+							alt=""
+						/>
+						<span>
+							{item.name} {item.gold.total}
+						</span>
+					</li>
 				))}
 			</ul>
 		</div>
--- types.d.ts
+++ types.d.ts
@@ -10,6 +10,7 @@ type Item = {
 	name: string;
 	description: string;
 	plaintext: string;
+	requiredChampion: string;
 	image: {
 		full: string;
 	};
@@ -18,5 +19,12 @@ type Item = {
 		purchasable: boolean;
 		total: number;
 		sell: number;
+	};
+	maps: {
+		"11": boolean;
+		"12": boolean;
+		"21": boolean;
+		"22": boolean;
+		"30": boolean;
 	};
 };
