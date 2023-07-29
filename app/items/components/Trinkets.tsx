70915354a4c7b5548c922cc0d74bfb147ace5c58
Ben Hur <Bhur777@gmail.com>
2023-07-28 23:58:51 -0400

    Trinket component done

--- app/items/components/Trinkets.tsx
+++ app/items/components/Trinkets.tsx
@@ -7,7 +7,7 @@ export default async function Trinkets({ items }: ItemProps) {
 			<div className="grid grid-cols-20 gap-3 text-white">
 				{items.map(
 					(item) =>
-						item.gold.total <= 450 && (
+						item.gold.total === 0 && (
 							<Image
 								className="border border-champBorder"
 								height={36}
