213865553e67ff1fc0a53b8ae63851d5633e6de4
Ben Hur <Bhur777@gmail.com>
2023-08-02 21:02:01 -0400

    Add borders and PQWER letters on abilities

--- app/champions/[championName]/page.tsx
+++ app/champions/[championName]/page.tsx
@@ -17,13 +17,14 @@ export async function generateMetadata({ params: { championName } }: Props) {
 export default async function ChampionPage({
 	params: { championName },
 }: Props) {
+	const hotkeys = ["Q", "W", "E", "R"];
 	const championData = getChampionData(championName);
 	const res = await championData;
 	return (
 		<div className="flex flex-col pl-20 mx-auto w-1/2 pt-32">
 			<div className="flex flex-row w-full items-top">
 				<Image
-					className=""
+					className="border border-champBorder"
 					height={100}
 					width={100}
 					src={`http://ddragon.leagueoflegends.com/cdn/13.14.1/img/champion/${res.data[championName].image.full}`}
@@ -43,14 +44,33 @@ export default async function ChampionPage({
 					</span>
 				</div>
 			</div>
-			<div className="text-white flex">
-				{res.data[championName].spells.map((spell: any) => (
+			<div className="ml-1 mt-10 p-4 text-white flex flex-row gap-2 w-full border border-champBorder bg-champBG justify-center">
+				<div className="flex flex-col items-center">
 					<Image
-						src={`http://ddragon.leagueoflegends.com/cdn/13.15.1/img/spell/${spell.image.full}`}
+						className="rounded-sm border border-itemBorder"
+						src={`http://ddragon.leagueoflegends.com/cdn/13.15.1/img/passive/${res.data[championName].passive.image.full}`}
 						height={48}
 						width={48}
 						alt=""
-					></Image>
+					/>
+					<span className="relative -top-2 border border-champBorder rounded-sm bg-champBG px-1 text-sm">
+						P
+					</span>
+				</div>
+
+				{res.data[championName].spells.map((spell: any) => (
+					<div className="flex flex-col items-center">
+						<Image
+							className="rounded-sm border border-itemBorder"
+							src={`http://ddragon.leagueoflegends.com/cdn/13.15.1/img/spell/${spell.image.full}`}
+							height={48}
+							width={48}
+							alt=""
+						/>
+						<span className="relative -top-2 border border-champBorder rounded-sm bg-champBG px-1 text-sm">
+							{spell.id.charAt(spell.id.length - 1)}
+						</span>
+					</div>
 				))}
 			</div>
 		</div>
