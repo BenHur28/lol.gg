da4f619af57b2911398dd0c2efc88da8061e3f04
Ben Hur <bhur777@gmail.com>
2023-08-12 19:01:50 -0400

    get ranked data

--- services/getSummoner.ts
+++ services/getSummoner.ts
@@ -1,5 +1,4 @@
 export default async function getSummoner(summonerName: string) {
-<<<<<<< HEAD
 	const summonerIdResponse = await fetch(
 		`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${process.env.DATA_API_KEY}`
 	);
@@ -10,10 +9,8 @@ export default async function getSummoner(summonerName: string) {
 	);
 
 	return responseRanked.json();
-=======
 	const response = await fetch(
 		`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${process.env.DATA_API_KEY}`
 	);
 	return response.json();
->>>>>>> 1864cf96976e28fd78cb01bf84be81651f5dae93
 }
