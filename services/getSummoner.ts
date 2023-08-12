41dd52d28677df1f23395ffcfbb7bc31428f63b7
Ben Hur <bhur777@gmail.com>
2023-08-12 19:02:33 -0400

    remove unreachable code

--- services/getSummoner.ts
+++ services/getSummoner.ts
@@ -9,8 +9,4 @@ export default async function getSummoner(summonerName: string) {
 	);
 
 	return responseRanked.json();
-	const response = await fetch(
-		`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${process.env.DATA_API_KEY}`
-	);
-	return response.json();
 }
