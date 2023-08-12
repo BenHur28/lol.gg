c787016d3bd949cddb072966028fbf2c81af437f
Ben Hur <bhur777@gmail.com>
2023-08-12 19:04:53 -0400

    Fetch summonerName Params

--- app/[summonerName]/page.tsx
+++ app/[summonerName]/page.tsx
@@ -1,7 +1,20 @@
 import getSummoner from "@/services/getSummoner";
 
-export default async function page() {
-	const summoner = getSummoner("Doublelift");
+type Props = {
+	params: {
+		summonerName: string;
+	};
+};
+
+export async function generateMetadata({ params: { summonerName } }: Props) {
+	return {
+		title: `${summonerName} Information`,
+		description: `Summoner page for ${summonerName}`,
+	};
+}
+
+export default async function page({ params: { summonerName } }: Props) {
+	const summoner = getSummoner(summonerName);
 	const data = await summoner;
 	console.log(data);
 	return <div className="pt-32 pl-40 mb-10 text-white">page</div>;
