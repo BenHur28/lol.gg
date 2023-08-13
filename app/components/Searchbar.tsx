9626b78d821f763a9854728f828180a296d40379
Ben Hur <bhur777@gmail.com>
2023-08-13 02:51:35 -0400

    Dropdowns in searchbar

--- app/components/Searchbar.tsx
+++ app/components/Searchbar.tsx
@@ -4,8 +4,27 @@ import { useRouter } from "next/navigation";
 
 export default function Searchbar() {
 	const [search, setSearch] = useState("");
-	const [open, setOpen] = useState(false);
+	const [open1, setOpen1] = useState(false);
+	const [open2, setOpen2] = useState(false);
 	const router = useRouter();
+	const regions = [
+		"NA",
+		"EUW",
+		"EUW",
+		"KR",
+		"BR",
+		"JP",
+		"RU",
+		"OCE",
+		"TR",
+		"LAN",
+		"LAS",
+		"PH",
+		"SG",
+		"TH",
+		"TW",
+		"VN",
+	];
 
 	const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
@@ -14,8 +33,12 @@ export default function Searchbar() {
 		}
 	};
 
-	const handleOpen = () => {
-		setOpen(!open);
+	const handleOpen1 = () => {
+		setOpen1(!open1);
+	};
+
+	const handleOpen2 = () => {
+		setOpen2(!open2);
 	};
 
 	return (
@@ -24,32 +47,46 @@ export default function Searchbar() {
 				<div className="flex max-[1024px]:mt-32">
 					<h1 className="text-white text-8xl mb-10">LOL.GG</h1>
 				</div>
-				<div className="text-white">
-					<button onClick={handleOpen}>click</button>
-					{open ? (
-						<ul className="">
-							<li className="">
-								<button>Summoners</button>
-							</li>
-							<li className="">
-								<button>Champions</button>
-							</li>
-						</ul>
-					) : null}
+				<div className="flex md:w-2/5">
+					<div className="relative top-4 left-16 z-10 text-black">
+						<button onClick={handleOpen1}>Search</button>
+						{open1 ? (
+							<ul className="">
+								<li className="">
+									<button>Summoners</button>
+								</li>
+								<li className="">
+									<button>Champions</button>
+								</li>
+							</ul>
+						) : null}
+					</div>
+					<form
+						onSubmit={handleSubmit}
+						className="flex flex-row w-full bg-white rounded-md py-4 px-2 justify-between"
+					>
+						<input
+							type="text"
+							value={search}
+							onChange={(e) => setSearch(e.target.value)}
+							className="mx-4 pl-20 w-full outline-none"
+							placeholder="Search Summoner or a Champion"
+						/>
+						<button>🔍</button>
+					</form>
+					<div className="relative top-4 right-24 z-10 text-black">
+						<button onClick={handleOpen2}>Region</button>
+						{open2 ? (
+							<ul className="">
+								{regions.map((region) => (
+									<li>
+										<button>{region}</button>
+									</li>
+								))}
+							</ul>
+						) : null}
+					</div>
 				</div>
-				<form
-					onSubmit={handleSubmit}
-					className="flex flex-row bg-white rounded-md py-4 px-2 md:w-1/3 justify-between"
-				>
-					<input
-						type="text"
-						value={search}
-						onChange={(e) => setSearch(e.target.value)}
-						className="mx-4 w-full outline-none"
-						placeholder="Search Summoner or a Champion"
-					/>
-					<button>🔍</button>
-				</form>
 			</div>
 		</div>
 	);
