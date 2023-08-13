fb14eba49ac9568a7e45cad85a5341fe48c4635e
Ben Hur <bhur777@gmail.com>
2023-08-12 23:43:11 -0400

    Display summoner stats

--- app/[summonerName]/page.tsx
+++ app/[summonerName]/page.tsx
@@ -1,4 +1,5 @@
 import getSummoner from "@/services/getSummoner";
+import Image from "next/image";
 
 type Props = {
 	params: {
@@ -17,6 +18,27 @@ export async function generateMetadata({ params: { summonerName } }: Props) {
 export default async function page({ params: { summonerName } }: Props) {
 	const summoner = getSummoner(summonerName);
 	const data = await summoner;
+	const winLossPercent = (data.wins / (data.wins + data.losses)) * 100;
 	console.log(data);
-	return <div className="pt-32 pl-40 mb-10 text-white">page</div>;
+	return (
+		<div className="pt-32 pl-40 mb-10 text-white">
+			<div className="flex flex-col items-center">
+				<h1>{summonerName}</h1>
+				<Image
+					className="border border-champBorder"
+					height={100}
+					width={100}
+					src={`http://ddragon.leagueoflegends.com/cdn/13.15.1/img/profileicon/${data.profileIconId}.png`}
+					alt=""
+				/>
+				<p>
+					{data.tier} {data.rank}
+				</p>
+				<p>
+					Wins: {data.wins} Loses: {data.losses}
+				</p>
+				<p>{Math.round(winLossPercent)}% Win Rate</p>
+			</div>
+		</div>
+	);
 }
