7aa26a0057a2e74d092f5e2fab78c20e6fef5e57
Ben Hur <bhur777@gmail.com>
2023-09-01 17:16:52 -0400

    Check for data and display accordingly

--- app/summoners/[summonerName]/[region]/page.tsx
+++ app/summoners/[summonerName]/[region]/page.tsx
@@ -37,12 +37,16 @@ export default async function page({
 	const normal_data = data[0].filter(
 		(d: { queueType: string }) => d.queueType == "CHERRY"
 	);
-	const rank = ranks.filter((rank) => rank.label == ranked_data[0].tier)[0]
-		.value;
-	const flex_rank = ranks.filter(
-		(rank) => rank.label == ranked_flex_data[0].tier
-	)[0].value;
+	const rank =
+		ranked_data.length > 0
+			? ranks.filter((rank) => rank.label == ranked_data[0].tier)[0].value
+			: "";
+	const flex_rank =
+		ranked_flex_data.length > 0
+			? ranks.filter((rank) => rank.label == ranked_flex_data[0].tier)[0].value
+			: "";
 	console.log(ranked_flex_data);
+	console.log(ranked_data);
 	return (
 		<div className="pt-40 mb-10 text-white">
 			<div className="flex flex-col items-center">
@@ -63,60 +67,89 @@ export default async function page({
 			<div className="mx-auto w-1/2 grid grid-cols-3 gap-4 mt-20">
 				<div className="col-span-1  ">
 					<div className="bg-matchHistory rounded-md px-4 py-2">
-						<h2 className="text-md font-semibold">Ranked Solo</h2>
-						<div className="flex justify-between mt-2">
-							{rank && <Image src={rank} alt="" height={40} width={40}></Image>}
-							<div className="flex flex-col">
-								<p className="text-xl font-bold">
-									{ranked_data[0].tier} {ranked_data[0].rank}
-								</p>
-								<span className="text-[#cddcfe] text-sm">
-									{ranked_data[0].leaguePoints} LP
-								</span>
+						{ranked_data.length == 0 && (
+							<div className="flex justify-between items-center">
+								<h2 className="text-md font-semibold">Ranked Solo</h2>
+								<span className="text-[#cddcfe] text-sm">Unranked</span>
 							</div>
-							<div className="flex flex-col justify-end">
-								<span className="text-end text-[#cddcfe] text-sm">
-									{ranked_data[0].wins}W {ranked_data[0].losses}L
-								</span>
-								<span className="text-end text-[#cddcfe] text-sm">
-									{Math.round(
-										(ranked_data[0].wins /
-											(ranked_data[0].wins + ranked_data[0].losses)) *
-											100
+						)}
+						{ranked_data.length > 0 && (
+							<>
+								<h2 className="text-md font-semibold">Ranked Solo</h2>
+								<div className="flex justify-between mt-2">
+									{rank && (
+										<Image src={rank} alt="" height={40} width={40}></Image>
 									)}
-									% Win Rate
-								</span>
-							</div>
-						</div>
+									<div className="flex flex-col">
+										<p className="text-xl font-bold">
+											{ranked_data[0].tier} {ranked_data[0].rank}
+										</p>
+										<span className="text-[#cddcfe] text-sm">
+											{ranked_data[0].leaguePoints} LP
+										</span>
+									</div>
+									<div className="flex flex-col justify-end">
+										<span className="text-end text-[#cddcfe] text-sm">
+											{ranked_data[0].wins}W {ranked_data[0].losses}L
+										</span>
+										<span className="text-end text-[#cddcfe] text-sm">
+											{Math.round(
+												(ranked_data[0].wins /
+													(ranked_data[0].wins + ranked_data[0].losses)) *
+													100
+											)}
+											% Win Rate
+										</span>
+									</div>
+								</div>
+							</>
+						)}
 					</div>
 					<div className="bg-matchHistory rounded-md mt-4 px-4 py-2">
-						<h2 className="text-md font-semibold">Ranked Flex</h2>
-						<div className="flex justify-between mt-2">
-							{flex_rank && (
-								<Image src={flex_rank} alt="" height={40} width={40}></Image>
-							)}
-							<div className="flex flex-col">
-								<p className="text-xl font-bold">
-									{ranked_flex_data[0].tier} {ranked_flex_data[0].rank}
-								</p>
-								<span className="text-[#cddcfe] text-sm">
-									{ranked_flex_data[0].leaguePoints} LP
-								</span>
+						{ranked_flex_data.length == 0 && (
+							<div className="flex justify-between items-center">
+								<h2 className="text-md font-semibold">Ranked Flex</h2>
+								<span className="text-[#cddcfe] text-sm">Unranked</span>
 							</div>
-							<div className="flex flex-col justify-end">
-								<span className="text-end text-[#cddcfe] text-sm">
-									{ranked_flex_data[0].wins}W {ranked_flex_data[0].losses}L
-								</span>
-								<span className="text-end text-[#cddcfe] text-sm">
-									{Math.round(
-										(ranked_flex_data[0].wins /
-											(ranked_flex_data[0].wins + ranked_flex_data[0].losses)) *
-											100
+						)}
+						{ranked_flex_data.length > 0 && (
+							<>
+								<h2 className="text-md font-semibold">Ranked Flex</h2>
+								<div className="flex justify-between mt-2">
+									{flex_rank && (
+										<Image
+											src={flex_rank}
+											alt=""
+											height={40}
+											width={40}
+										></Image>
 									)}
-									% Win Rate
-								</span>
-							</div>
-						</div>
+									<div className="flex flex-col">
+										<p className="text-xl font-bold">
+											{ranked_flex_data[0].tier} {ranked_flex_data[0].rank}
+										</p>
+
+										<span className="text-[#cddcfe] text-sm">
+											{ranked_flex_data[0].leaguePoints} LP
+										</span>
+									</div>
+									<div className="flex flex-col justify-end">
+										<span className="text-end text-[#cddcfe] text-sm">
+											{ranked_flex_data[0].wins}W {ranked_flex_data[0].losses}L
+										</span>
+										<span className="text-end text-[#cddcfe] text-sm">
+											{Math.round(
+												(ranked_flex_data[0].wins /
+													(ranked_flex_data[0].wins +
+														ranked_flex_data[0].losses)) *
+													100
+											)}
+											% Win Rate
+										</span>
+									</div>
+								</div>
+							</>
+						)}
 					</div>
 				</div>
 				<div className="col-span-2 text-xl font-semibold p-4 bg-[#11112a] rounded-md">
