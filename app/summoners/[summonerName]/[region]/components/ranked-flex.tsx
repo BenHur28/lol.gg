ab5f9b5983eb3b5968ff67886938613db03a39d7
Ben Hur <bhur777@gmail.com>
2023-09-02 19:57:13 -0400

    refactor ranked flex component

--- app/summoners/[summonerName]/[region]/components/ranked-flex.tsx
+++ app/summoners/[summonerName]/[region]/components/ranked-flex.tsx
@@ -1,28 +1,29 @@
 import Image from "next/image";
 
-type RankedSoloProps = {
+type RankedFlexProps = {
 	rank: string;
 	data: any[];
 };
 
-const RankedSolo = ({ data, rank }: RankedSoloProps) => {
+const RankedFlex = ({ data, rank }: RankedFlexProps) => {
 	return (
-		<div className="bg-matchHistory rounded-md px-4 py-2">
+		<div className="bg-matchHistory rounded-md mt-4 px-4 py-2">
 			{data.length == 0 && (
 				<div className="flex justify-between items-center">
-					<h2 className="text-md font-semibold">Ranked Solo</h2>
+					<h2 className="text-md font-semibold">Ranked Flex</h2>
 					<span className="text-[#cddcfe] text-sm">Unranked</span>
 				</div>
 			)}
 			{data.length > 0 && (
 				<>
-					<h2 className="text-md font-semibold">Ranked Solo</h2>
+					<h2 className="text-md font-semibold">Ranked Flex</h2>
 					<div className="flex justify-between mt-2">
 						{rank && <Image src={rank} alt="" height={40} width={40}></Image>}
 						<div className="flex flex-col">
 							<p className="text-xl font-bold">
 								{data[0].tier} {data[0].rank}
 							</p>
+
 							<span className="text-[#cddcfe] text-sm">
 								{data[0].leaguePoints} LP
 							</span>
@@ -45,4 +46,4 @@ const RankedSolo = ({ data, rank }: RankedSoloProps) => {
 	);
 };
 
-export default RankedSolo;
+export default RankedFlex;
--- app/summoners/[summonerName]/[region]/page.tsx
+++ app/summoners/[summonerName]/[region]/page.tsx
@@ -1,10 +1,10 @@
 import { cn } from "@/lib/utils";
 import getMatchHistory from "@/services/getMatchHistory";
 import getSummoner from "@/services/getSummoner";
-import Image from "next/image";
 import { ranks } from "@/lib/data";
 import Summoner from "./components/summoner";
 import RankedSolo from "./components/ranked-solo";
+import RankedFlex from "./components/ranked-flex";
 
 type Props = {
 	params: {
@@ -52,53 +52,8 @@ export default async function page({
 			<Summoner name={summonerName} image={data[1]}></Summoner>
 			<div className="mx-auto w-1/2 grid grid-cols-3 gap-4 mt-20">
 				<div className="col-span-1  ">
-					<RankedSolo data={ranked_data} rank={rank}></RankedSolo>
-					<div className="bg-matchHistory rounded-md mt-4 px-4 py-2">
-						{ranked_flex_data.length == 0 && (
-							<div className="flex justify-between items-center">
-								<h2 className="text-md font-semibold">Ranked Flex</h2>
-								<span className="text-[#cddcfe] text-sm">Unranked</span>
-							</div>
-						)}
-						{ranked_flex_data.length > 0 && (
-							<>
-								<h2 className="text-md font-semibold">Ranked Flex</h2>
-								<div className="flex justify-between mt-2">
-									{flex_rank && (
-										<Image
-											src={flex_rank}
-											alt=""
-											height={40}
-											width={40}
-										></Image>
-									)}
-									<div className="flex flex-col">
-										<p className="text-xl font-bold">
-											{ranked_flex_data[0].tier} {ranked_flex_data[0].rank}
-										</p>
-
-										<span className="text-[#cddcfe] text-sm">
-											{ranked_flex_data[0].leaguePoints} LP
-										</span>
-									</div>
-									<div className="flex flex-col justify-end">
-										<span className="text-end text-[#cddcfe] text-sm">
-											{ranked_flex_data[0].wins}W {ranked_flex_data[0].losses}L
-										</span>
-										<span className="text-end text-[#cddcfe] text-sm">
-											{Math.round(
-												(ranked_flex_data[0].wins /
-													(ranked_flex_data[0].wins +
-														ranked_flex_data[0].losses)) *
-													100
-											)}
-											% Win Rate
-										</span>
-									</div>
-								</div>
-							</>
-						)}
-					</div>
+					<RankedSolo data={ranked_data} rank={rank} />
+					<RankedFlex data={ranked_flex_data} rank={flex_rank} />
 				</div>
 				<div className="col-span-2 text-xl font-semibold p-4 bg-[#11112a] rounded-md">
 					{matches.map((match: any, index: number) => (
