b29934eb8fb6e390d45682267c7d3f2ff0159712
Ben Hur <bhur777@gmail.com>
2023-09-05 20:21:16 -0400

    Check to see if item is empty. Render empty block

--- app/summoners/[summonerName]/[region]/components/player-items.tsx
+++ app/summoners/[summonerName]/[region]/components/player-items.tsx
@@ -1,3 +1,4 @@
+import { Divide } from "lucide-react";
 import Image from "next/image";
 
 type PlayeritemsProps = {
@@ -6,45 +7,36 @@ type PlayeritemsProps = {
 
 const Playeritems = ({ player }: PlayeritemsProps) => {
 	const playerData = player[0];
+	const items: string[] = [];
+	const pushItems = () => {
+		items.push(playerData.item0);
+		items.push(playerData.item1);
+		items.push(playerData.item2);
+		items.push(playerData.item3);
+		items.push(playerData.item4);
+		items.push(playerData.item5);
+	};
+	pushItems();
+	console.log(items);
 	return (
 		<div className="flex justify-center my-auto pb-8">
 			<div className="grid grid-cols-3 h-0 gap-0.5">
-				<Image
-					height={22}
-					width={22}
-					src={`https://ddragon.leagueoflegends.com/cdn/13.17.1/img/item/${playerData.item0}.png`}
-					alt=""
-				/>
-				<Image
-					height={22}
-					width={22}
-					src={`https://ddragon.leagueoflegends.com/cdn/13.17.1/img/item/${playerData.item1}.png`}
-					alt=""
-				/>
-				<Image
-					height={22}
-					width={22}
-					src={`https://ddragon.leagueoflegends.com/cdn/13.17.1/img/item/${playerData.item2}.png`}
-					alt=""
-				/>
-				<Image
-					height={22}
-					width={22}
-					src={`https://ddragon.leagueoflegends.com/cdn/13.17.1/img/item/${playerData.item3}.png`}
-					alt=""
-				/>
-				<Image
-					height={22}
-					width={22}
-					src={`https://ddragon.leagueoflegends.com/cdn/13.17.1/img/item/${playerData.item4}.png`}
-					alt=""
-				/>
-				<Image
-					height={22}
-					width={22}
-					src={`https://ddragon.leagueoflegends.com/cdn/13.17.1/img/item/${playerData.item5}.png`}
-					alt=""
-				/>
+				{items.map((item: string) =>
+					item != "0" ? (
+						<Image
+							key={item}
+							height={22}
+							width={22}
+							src={`https://ddragon.leagueoflegends.com/cdn/13.17.1/img/item/${item}.png`}
+							alt=""
+						/>
+					) : (
+						<div
+							key={item}
+							className="h-22 w-22 bg-[#3273fa] shadow-md rounded-sm"
+						></div>
+					)
+				)}
 			</div>
 			<Image
 				className="self-start ml-0.5"
