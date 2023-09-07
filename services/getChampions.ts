a6129cbada3d9ea96bad938356799bdb6ccb8b84
Ben Hur <bhur777@gmail.com>
2023-09-06 21:15:51 -0400

    update api fetch routes

--- services/getChampions.ts
+++ services/getChampions.ts
@@ -1,6 +1,6 @@
 export default async function getChampionsData() {
 	const response = await fetch(
-		"http://ddragon.leagueoflegends.com/cdn/13.14.1/data/en_US/champion.json"
+		"http://ddragon.leagueoflegends.com/cdn/13.17.1/data/en_US/champion.json"
 	);
 
 	return response.json();
--- services/getItems.ts
+++ services/getItems.ts
@@ -1,6 +1,6 @@
 export default async function getItemsData() {
 	const response = await fetch(
-		"http://ddragon.leagueoflegends.com/cdn/13.14.1/data/en_US/item.json"
+		"http://ddragon.leagueoflegends.com/cdn/13.17.1/data/en_US/item.json"
 	);
 	return response.json();
 }
