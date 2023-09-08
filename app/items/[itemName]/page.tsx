09af98c1e9d657a0c7f0db8e9b9647ac062e4946
Ben Hur <bhur777@gmail.com>
2023-09-07 22:46:31 -0400

    Display single item

--- app/items/[itemName]/page.tsx
+++ app/items/[itemName]/page.tsx
@@ -1,4 +1,6 @@
+import getItemsData from "@/services/getItems";
 import React from "react";
+import Image from "next/image";
 
 type Props = {
 	params: {
@@ -14,5 +16,28 @@ export async function generateMetadata({ params: { itemName } }: Props) {
 }
 
 export default async function page({ params: { itemName } }: Props) {
-	return <div>{itemName}</div>;
+	const itemData = getItemsData();
+	const res = await itemData;
+	const singleItem: Item[] = [];
+	for (let item in res.data) {
+		if (res.data[item].name == itemName.replace(/%20/g, " ")) {
+			singleItem.push(res.data[item]);
+		}
+	}
+	return (
+		<div className="pt-32 w-2/5 mx-auto">
+			<div className="flex items-center">
+				<Image
+					className="border border-itemBorder rounded-sm"
+					height={36}
+					width={36}
+					src={`http://ddragon.leagueoflegends.com/cdn/13.14.1/img/item/${singleItem[0].image.full}`}
+					alt=""
+				/>
+				<div className="text-2xl text-white">
+					{itemName.replace(/%20/g, " ")}
+				</div>
+			</div>
+		</div>
+	);
 }
