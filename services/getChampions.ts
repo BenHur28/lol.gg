533f9c6788aa5f27ee0a3bd9aec60b275d601c68
BenHur28 <bhur777@gmail.com>
2024-02-10 03:30:56 -0500

    Update patch to 14.13.1

--- services/getChampions.ts
+++ services/getChampions.ts
@@ -1,6 +1,6 @@
 export default async function getChampionsData() {
 	const response = await fetch(
-		"http://ddragon.leagueoflegends.com/cdn/13.17.1/data/en_US/champion.json"
+		"https://ddragon.leagueoflegends.com/cdn/14.3.1/data/en_US/champion.json"
 	);
 
 	return response.json();
