3123bb7fd40fde85a9dc648d8b2db7b235b68ced
BenHur28 <bhur777@gmail.com>
2024-03-01 00:31:04 -0500

    add work break to consumable items

--- app/items/components/ConsumableItems.tsx
+++ app/items/components/ConsumableItems.tsx
@@ -4,7 +4,7 @@ export default async function ConsumableItems({ items }: ItemProps) {
 	return (
 		<div className="mt-8 p-4 rounded-sm bg-champBG">
 			<h1 className="text-start mb-4">Consumable Items</h1>
-			<div className="grid min-[320px]:grid-cols-4 min-[375px]:grid-cols-6 min-[768px]:grid-cols-10 min-[1024px]:grid-cols-16 min-[1440px]:grid-cols-20 gap-3 text-white">
+			<div className="grid min-[320px]:grid-cols-4 min-[375px]:grid-cols-6 min-[768px]:grid-cols-10 min-[1024px]:grid-cols-16 min-[1440px]:grid-cols-20 gap-3 text-white break-words">
 				{items.map(
 					(item) =>
 						item.tags?.includes("Consumable") && (
