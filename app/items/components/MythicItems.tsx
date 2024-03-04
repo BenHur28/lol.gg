3bb48ec2f50279a41129176faaac0be171dc9096
BenHur28 <bhur777@gmail.com>
2024-03-04 10:53:59 -0500

    add world break to mythic items

--- app/items/components/MythicItems.tsx
+++ app/items/components/MythicItems.tsx
@@ -4,7 +4,7 @@ export default async function MythicItems({ items }: ItemProps) {
 	return (
 		<div className="my-8 p-4 rounded-sm bg-champBG">
 			<h1 className="text-start mb-4">Expensive Items</h1>
-			<div className="grid min-[320px]:grid-cols-4 min-[375px]:grid-cols-6 min-[768px]:grid-cols-10 min-[1024px]:grid-cols-16 min-[1440px]:grid-cols-20 gap-3 text-white">
+			<div className="grid min-[320px]:grid-cols-4 min-[375px]:grid-cols-6 min-[768px]:grid-cols-10 min-[1024px]:grid-cols-16 min-[1440px]:grid-cols-20 gap-3 text-white break-words">
 				{items.map(
 					(item) =>
 						((item.depth == 3 &&
