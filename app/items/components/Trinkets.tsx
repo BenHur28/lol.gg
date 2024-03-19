57d769a1e39a056d83208442bf3cd500425626a6
BenHur28 <bhur777@gmail.com>
2024-03-19 15:59:46 -0400

    Change trinkets component patch to 14.5.1

--- app/items/components/Trinkets.tsx
+++ app/items/components/Trinkets.tsx
@@ -15,7 +15,7 @@ export default async function Trinkets({ items }: ItemProps) {
 										className="border border-itemBorder rounded-sm"
 										height={36}
 										width={36}
-										src={`http://ddragon.leagueoflegends.com/cdn/14.4.1/img/item/${item.image.full}`}
+										src={`http://ddragon.leagueoflegends.com/cdn/14.5.1/img/item/${item.image.full}`}
 										alt=""
 									/>
 								</Link>
