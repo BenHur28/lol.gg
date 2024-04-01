56736b95ed437e21d0e25fdc8838c7aef2825150
BenHur28 <bhur777@gmail.com>
2024-04-01 05:34:05 -0400

    14.6.1. champion data

--- services/getChampion.ts
+++ services/getChampion.ts
@@ -1,7 +1,7 @@
 export default async function getChampionData(championName: string) {
 	const search = championName.charAt(0).toUpperCase() + championName.slice(1);
 	const response = await fetch(
-		`https://ddragon.leagueoflegends.com/cdn/14.5.1/data/en_US/champion/${search}.json`
+		`https://ddragon.leagueoflegends.com/cdn/14.6.1/data/en_US/champion/${search}.json`
 	);
 
 	return response.json();
