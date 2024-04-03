c8d40f7acb34d628e41a3cddb98ea9acbce29243
BenHur28 <bhur777@gmail.com>
2024-04-03 00:30:42 -0400

    get items 14.6.1

--- services/getItems.ts
+++ services/getItems.ts
@@ -1,6 +1,6 @@
 export default async function getItemsData() {
 	const response = await fetch(
-		"http://ddragon.leagueoflegends.com/cdn/14.5.1/data/en_US/item.json"
+		"http://ddragon.leagueoflegends.com/cdn/14.6.1/data/en_US/item.json"
 	);
 	return response.json();
 }
